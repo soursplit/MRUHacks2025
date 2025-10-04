99c7ca9087377f13cc7fa72013fa6ab1ea947e3b
Nelson Wing <dwing998@mtroyal.ca>
2025-10-04 15:45:49 -0600

    commit

---
+++ P2rigid_player.gd
@@ -0,0 +1,23 @@
+extends RigidBody2D
+
+var move_direction = Vector2.ZERO
+var speed = 500
+
+func _physics_process(delta):
+	# Get input and update move_direction
+	move_direction = Vector2.ZERO
+	if Input.is_action_pressed("P2 Right"):
+		move_direction.x += 1
+	if Input.is_action_pressed("P2 Left"):
+		move_direction.x -= 1
+	if Input.is_action_pressed("P2 Up"):
+		move_direction.y -= 1
+	if Input.is_action_pressed("P2 Down"):
+		move_direction.y += 1
+	
+	# Normalize the direction to prevent faster diagonal movement
+	move_direction = move_direction.normalized()
+
+func _integrate_forces(state):
+	# Apply a force based on input
+	state.apply_central_force(move_direction * speed) 
