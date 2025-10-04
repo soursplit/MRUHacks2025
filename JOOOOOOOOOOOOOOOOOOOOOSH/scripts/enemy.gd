fb43e54d4f1cbd1294e186d25c8883a7a602fe75
Insert-Name-Here-Josh <knaci232@mtroyal.ca>
2025-10-04 16:26:30 -0600

    added enemy health

--- JOOOOOOOOOOOOOOOOOOOOOSH/scripts/enemy.gd
+++ JOOOOOOOOOOOOOOOOOOOOOSH/scripts/enemy.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 ##Code inspired from "Create AI Enemies in Godot4 2D | Godot simple enemy tutorial ." by GameStick
 @onready var target = $"../Player"
 var speed = 150
+var health: int =  1
 
 ## Handles physics of moving enemy
 func _physics_process(delta):
@@ -11,3 +12,10 @@ func _physics_process(delta):
 	look_at(target.position) ## Makes enemy look at chosen player
 	move_and_slide() ## Function that handles smooth movement
 ##Code inspired from "Create AI Enemies in Godot4 2D | Godot simple enemy tutorial ." by GameStick
+
+func take_damage(damage:int):
+	health -= damage
+	print("Damagedwa")
+	if health <= 0:
+		$Sprite2D.visible = false
+		print("Dead")
