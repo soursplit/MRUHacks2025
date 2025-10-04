590bdbce7a298ed38950dd8d17e7f237c2380aee
Insert-Name-Here-Josh <knaci232@mtroyal.ca>
2025-10-04 17:50:54 -0600

    added base damage and enemy logic

---
+++ Nelson/Scripts/chain_damage.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+## Code Inspired by "Damage System for Beginner - Learn Godot 4 = no talking" by DevDrache
+func _on_body_entered(body : CharacterBody2D):
+	if "Player" in body.name:
+		body.take_damage(1)
+## End of Code Inspired by "Damage System for Beginner - Learn Godot 4 = no talking" by DevDrache
+
+## Made for the chain, is slightly bigger than chain so collision hits
