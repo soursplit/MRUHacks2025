267ddde165feb7f8a63f11e5ecec520a6ce82aaa
Shervin Farzamirad <sfarz913@mtroyal.ca>
2025-10-04 19:46:40 -0600

    ghasdjkafhjksd

--- Nelson/area_2d.gd
+++ Nelson/area_2d.gd
@@ -2,4 +2,5 @@ extends Area2D
 
 
 func _on_body_entered(body):
-	get_tree().change_scene_to_file("res://level_2.tscn")
+	if body.is_in_group("Players"):
+		get_tree().change_scene_to_file("res://level_2.tscn")
--- Nelson/door_3.gd
+++ Nelson/door_3.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 
+
 func _on_body_entered(body):
 	if body.is_in_group("Players"):
-		get_tree().change_scene_to_file("res://Level 3.tscn")
+		get_tree().change_scene_to_file("res://Victory Screen.tscn")
